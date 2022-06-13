72057a25f1bd4efeb0d33e41358d5e25b94c8ad1
Hallcoder <hallcoder25@gmail.com>
2022-06-13 15:55:27 +0200

    menu:fetch menu & get user data in navbar

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,185 +1,162 @@
-import React from 'react';
-import { MdAddBox } from 'react-icons/md';
-import { FiSearch } from 'react-icons/fi';
-import { MdOutlineNotificationAdd } from 'react-icons/md';
-import { GoPerson } from 'react-icons/go';
-import Buttons from './Buttons';
-import pic from '../Assets/food.jpg'
-import Sidebar from '../components/Sidebar';
-import axios from 'axios';
+import React from "react";
+import { MdAddBox } from "react-icons/md";
+import { FiSearch } from "react-icons/fi";
+import { MdOutlineNotificationAdd } from "react-icons/md";
+import { GoPerson } from "react-icons/go";
+import Buttons from "./Buttons";
+import pic from "../Assets/food.jpg";
+import Sidebar from "../components/Sidebar";
+import axios from "axios";
+import { useEffect, useState } from "react";
+const api = axios.create({
+  baseURL: "https://backend.supamenu.rw/supapp",
+});
+//  const getMenu = (box,content) =>{
 
-// const data=  axios.get('https://backend.supamenu.rw/supapp/swagger-ui.html#/menu-item-controller/getAllUsingGET_1')
-//         data.then(response => {
-//             console.log(response.data);
-//         })
-//         .catch(err => {
-//             console.log(err);
-//         })
-        
+//  }
 
-async function getUser(){
-    const data = await axios.get('https://backend.supamenu.rw/api/menu-items');
-    const items = await data.json();
-    console.log(items);
-}
-getUser();
-
-
-
-
-
-export const box = [{
-    items: ['kaffir', 'vodka', 'Lemongrass', 'Ginger', 'citrus'],
-    selected: 'Tommy-yummy-12.5',
-    amount: 'FRW 5,000',
-    status:"Drink"
-}, {
-    items: ['Gin', 'Grenadine', 'Citrus', 'Cucumber'],
-    selected: 'Singapore Sling-12.5',
-    amount: 'FRW 5,000',
-    status:"Starter"
-
-}, {
-    items: ['Gin', 'Grenadine', 'Citrus', 'Cucumber'],
-    selected: 'Singapore Sling-12.5',
-    amount: 'FRW 5,000',
-    status:"Appetizer"
-
-}, {
-    items: ['Gin', 'Grenadine', 'Citrus', 'Cucumber'],
-    selected: 'Singapore Sling-12.5',
-    amount: 'FRW 5,000',
-    status:"Dessert"
-
-},
-{
-    items: ['Gin', 'Grenadine', 'Citrus', 'Cucumber'],
-    selected: 'Singapore Sling-12.5',
-    amount: 'FRW 5,000',
-    status:"Main"
-
-
-}
+const box2 = [
+  {
+    items: ["kaffir", "vodka", "Lemongrass", "Ginger", "citrus"],
+    selected: "Tommy-yummy-12.5",
+    amount: "FRW 5,000",
+    status: "Drink",
+  },
+  {
+    items: ["Gin", "Grenadine", "Citrus", "Cucumber"],
+    selected: "Singapore Sling-12.5",
+    amount: "FRW 5,000",
+    status: "Starter",
+  },
+  {
+    items: ["Gin", "Grenadine", "Citrus", "Cucumber"],
+    selected: "Singapore Sling-12.5",
+    amount: "FRW 5,000",
+    status: "Appetizer",
+  },
+  {
+    items: ["Gin", "Grenadine", "Citrus", "Cucumber"],
+    selected: "Singapore Sling-12.5",
+    amount: "FRW 5,000",
+    status: "Dessert",
+  },
+  {
+    items: ["Gin", "Grenadine", "Citrus", "Cucumber"],
+    selected: "Singapore Sling-12.5",
+    amount: "FRW 5,000",
+    status: "Main",
+  },
 ];
-const recipes = ['Desert', 'Main', 'Drink', 'Appetizer', 'Starter'];
-
-
-
-
+const recipes = ["Desert", "Main", "Drink", "Appetizer", "Starter"];
 
 function Menu() {
-    return (
-        <div className='flex flex-row h-screen w-full bg-white  px-2'>
-            <Sidebar active="Menu" />
-            <div className='w-5/6  h-screen block ml-64  '>
-                <div className=' flex flex-row px-4 h-1/6 py-8 justify-between  '>
-                    <p className=' text-2xl font-semibold text-black ml-40 mt-8 '>Menu</p>
-                    <div className='left flex flex-row px-8 justify-around'>
-                        <FiSearch className='mr-4 text-xl mt-1   text-indigo-400' />
-                        <MdOutlineNotificationAdd className='text-xl mr-4 mt-1 text-indigo-400' />
-                        <p className='text-xl font-normal  ml-8 '>Mugabo Javis</p>
-                        <GoPerson className='mr-4 text-xl mt-1 ml-4   outline-indigo-100' />
-
-                    </div>
-
-
-
-                </div>
-                <div className=' block w-5/6 bg-white shadow-xl shadow-indigo-xl h-6/6 ml-40'>
-                    <div className='flex flex-row ml-8 space-x-4 space-y-6'>
-                        <p className='text-xl font-semibold text-black ml-6 mr-12 mt-8'>Menus</p>
-
-                        <Buttons activity="Drinks" />
-
-
-                    </div>
-
-
-                    <div className='flex flex-row justify-around'>
-                        <div className='block space-y-4  py-3 px-2 w-8/12 h-5/6 '>
-                            {box.map((wer,index) => {
-
-                                return (
-
-                                    <div key={index} className='bg-[FCF8F8]  border-3 p-2 border-indigo-70 rounded-lg text-center flex flex-row w-6/6 h-24 shadow-md shadow-indigo-100'>
-                                        <div className='w-3/12 h-full'>
-                                            <img src={pic} className=' w-full h-full ml-1 rounded-xs object-cover rounded-lg' alt="food" />
-                                        </div>
-                                        <div className='  h-10 space-x-1 w-full  pl-4  flex flex-col items-start justify-start'>{}
-                                            <p className='block  w-3/4  text-gray-500 text-xs items-stretch text-left tracking-wide object-scale-down ' >{wer.items + " "}</p>
-                                            <p className='block  text-black font-semibold text-md' >{wer.selected}</p>
-                                            <p className='block  text-orange-400 font-semibold text-md text-stretch  ' >{wer.amount}</p>
-                                        </div>
-                                    </div>
-                                )
-
-                            })}
-
-
-                        </div>
-                        <div className='block  border-2 h-4/6 shadow-md shadow-indigo-100 mt-4 w-3/12'>
-                            <p className='font-semibold text-xl text-left pl-6  h-1/6'>Add Item</p>
-                            <div className='flex flex-row justify-between pt-2 mr-2 h-1/6'>
-                                <input className='text-xl text-black  pl-6 bg-white focus:ring-indigo-100 border-1 border-indigo-100 mb-2 ' placeholder='Create new menu item' />
-
-                                <MdAddBox className='text-indigo-200 text-3xl mr-2 mt-3 hover:cursor-pointer transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110  duration-300' />
-                            </div>
-                            <div className="grid grid-cols-1 divide-y space-y-1 pt-1 h-4/6">
-                                {
-
-                                    recipes.map((ing, index) => {
-                                        if (index === 2) {
-
-                                            return (
-
-
-                                                <div key={index} className='flex flex-row justify-left px-6 '>
-
-
-                                                  
-                                                     <input type="checkbox" />
-
-                                                    <div className='text-xl' key={index}>{ing}</div>
-                                                </div>
-                                            )
-                                        }
-                                        else {
-                                            return (
-                                                <div  className='flex flex-row justify-left px-6 '>
-
-                                                    {/* <BsCircle className='text-indigo-400  mt-2 mr-4 ' /> */}
-                                                    <input type="checkbox"  />
-
-
-                                                    <div className='text-xl' key={index}>{ing}</div>
-                                                </div>
-
-                                            )
-                                        }
-                                    })
-                                }
-
-
-
-
-                            </div>
-
-
-
-
-
-                        </div>
-
-
-
-
-                    </div>
-
-                </div>
-            </div>
+  let i = 0;
+  const [content12, setContent] = useState([]);
+    useEffect(()=> {
+        api.get("/api/menu-items")
+        .then(function (response) {
+          let contents = response.data.content;
+          setContent(contents);
+          console.log(content12);
+        })
+        .catch(function (err) {
+          console.log(err);
+        });
+    },[])
+    const {firstName} = JSON.parse(localStorage.getItem("userData"));
+  return (
+      <div className="flex flex-row h-screen w-full bg-white  px-2">
+        {console.log(content12)}
+      <Sidebar active="Menu"   />
+      <div className="w-5/6  h-screen block ml-64  ">
+        <div className=" flex flex-row px-4 h-1/6 py-8 justify-between  ">
+          <p className=" text-2xl font-semibold text-black ml-40 mt-8 ">Menu</p>
+          <div className="left flex flex-row px-8 justify-around">
+            <FiSearch className="mr-4 text-xl mt-1   text-indigo-400" />
+            <MdOutlineNotificationAdd className="text-xl mr-4 mt-1 text-indigo-400" />
+            <p className="text-xl font-normal  ml-8 ">{firstName}</p>
+            <GoPerson className="mr-4 text-xl mt-1 ml-4   outline-indigo-100" />
+          </div>
         </div>
-    )
-};
+        <div className=" block w-5/6 bg-white shadow-xl shadow-indigo-xl h-6/6 ml-40">
+          <div className="flex flex-row ml-8 space-x-4 space-y-6">
+            <p className="text-xl font-semibold text-black ml-6 mr-12 mt-8">
+              Menus
+            </p>
+            <Buttons activity="Drinks" />
+          </div>
+          <div className="flex flex-row justify-around">
+            <div className="block space-y-4  py-3 px-2 w-8/12 h-5/6 ">
+              {content12.map((content) => {
+               return <div
+                  key={content.id}
+                  className="bg-[FCF8F8]  border-3 p-2 border-indigo-70 rounded-lg text-center flex flex-row w-6/6 h-24 shadow-md shadow-indigo-100"
+                >
+                  <div className="w-3/12 h-full">
+                    <img
+                      src={pic}
+                      className=" w-full h-full ml-1 rounded-xs object-cover rounded-lg"
+                      alt="food"
+                    />
+                  </div>
+                  <div className="h-10 space-x-1 w-full  pl-4  flex flex-col items-start justify-start">
+                    <p className="block  w-3/4  text-gray-500 text-xs items-stretch text-left tracking-wide object-scale-down ">
+                      {box2[i].items + " "}
+                    </p>
+                    <p className="block  text-black font-semibold text-md">
+                      {content.name}
+                    </p>
+                    <p className="block  text-orange-400 font-semibold text-md text-stretch  ">
+                      {content.unitPrice}
+                    </p>
+                  </div>
+                </div>;
+              })}
+            </div>
+            <div className="block  border-2 h-4/6 shadow-md shadow-indigo-100 mt-4 w-3/12">
+              <p className="font-semibold text-xl text-left pl-6  h-1/6">
+                Add Item
+              </p>
+              <div className="flex flex-row justify-between pt-2 mr-2 h-1/6">
+                <input
+                  className="text-xl text-black  pl-6 bg-white focus:ring-indigo-100 border-1 border-indigo-100 mb-2 "
+                  placeholder="Create new menu item"
+                />
+                <MdAddBox className="text-indigo-200 text-3xl mr-2 mt-3 hover:cursor-pointer transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110  duration-300" />
+              </div>
+              <div className="grid grid-cols-1 divide-y space-y-1 pt-1 h-4/6">
+                {recipes.map((ing, index) => {
+                  if (index === 2) {
+                    return (
+                      <div
+                        key={index}
+                        className="flex flex-row justify-left px-6 "
+                      >
+                        <input type="checkbox" />
 
+                        <div className="text-xl">{ing}</div>
+                      </div>
+                    );
+                  } else {
+                    return (
+                      <div
+                        key={index}
+                        className="flex flex-row justify-left px-6 "
+                      >
+                        {/* <BsCircle className='text-indigo-400  mt-2 mr-4 ' /> */}
+                        <input type="checkbox" />
+                        <div className="text-xl">{ing}</div>
+                      </div>
+                    );
+                  }
+                })}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default Menu;
--- src/pages/login.js
+++ src/pages/login.js
@@ -2,39 +2,42 @@ import React,{useState} from "react";
 import "../css/Login.css";
 import Food from "../Assets/food.jpg";
 import {TextField} from "@mui/material"
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 const Login = () => {
   const api = axios.create({
-    baseURL:'https://backend.supamenu.rw'
+    baseURL:'https://backend.supamenu.rw/supapp'
   })
-  
+  const navigate = useNavigate()
   let[account,setAccount] = useState({email:"",password:""});
   const handleChange = ({currentTarget:input}) =>{
        console.log(input.name)
        let account1 = {...account};
        account1[input.name] = input.value
        account = account1
-       setAccount({account})
+       setAccount(account)
+       console.log(account)
+ 
   }
   const loginHandler = (event)=>{
   event.preventDefault();
-  // const data = {
-  //   email:account.email,
-  //   password:account.password,
-  // }
-  api.post("/supapp/api/auth/signin",{
+  console.log(account)
+  api.post("/api/auth/signin",{
     "login":account.email,
     "password":account.password
   })
   .then(function (response){
+    console.log(response.data)
     localStorage.setItem("accessToken",response.data.token.accessToken);
+    localStorage.setItem("userData",JSON.stringify(response.data))
+    // console.log(JSON.parse(localStorage.getItem("userData")).firstName)
     localStorage.setItem("refreshToken",response.data.token.refreshToken);
+    navigate("/overview")
   })
   .catch(function (error){
-    const message = error.response.data.apierror.message
-    JSON.stringify(message)
+    console.log(error);
+    // JSON.stringify(message)
   })
   }
   return (
@@ -50,6 +53,7 @@ const Login = () => {
             <p className="paras"> Eat well and happily</p>
             <div>
               <div className="form12">
+                <form onSubmit={(e) => loginHandler(e)}>
               <div className="password">
                 <TextField id="input" name="email" label="E-mail" value={account.email} variant="outlined" onChange={handleChange} />
               </div>
@@ -58,8 +62,9 @@ const Login = () => {
               </div>
               
               <div className="login">
-               <Link to="/overview"> <button className="button" onClick={loginHandler}>LOGIN</button></Link>
+             <input type="submit" className="button text-center" value="LOGIN"/>
               </div>
+              </form>
               <div className="footer">
                 <h5 className="ml-32">
                  Don't have an account here? <Link to="/signup" className="text-orange-400 hover:underline hover:cursor-pointer">Signup</Link></h5>
